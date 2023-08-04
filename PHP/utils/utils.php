d16ddcf7f094dd0c362cebb58b90d052e3955581
Kristian Røste <github@roste.org>
2023-08-04 12:31:55 +0200

    added some useful functions

--- PHP/utils/utils.php
+++ PHP/utils/utils.php
@@ -8,6 +8,32 @@ class util {
         return $paramCount;
     }
 
+    function getCurrentTime(string $format, string $timezone) : string {
+        $dt = new DateTime('now');
+        $tz = new DateTimeZone($timezone);
+        $dt->setTimeZone($tz);
+        $return = $dt->format($format);
+    
+        return $return;
+    }
+
+    function var_assert(mixed &$var, mixed $assertVal = false, bool $lazy = false) : bool {
+        if (!isset($var)) {
+            return false;
+        }
+    
+        if ($assertVal != false || func_num_args() > 1) {
+    
+            if ($lazy != false) {
+                return $var == $assertVal;
+            }
+    
+            return $var === $assertVal;
+        }
+        
+        return true;
+    }
+
 }
 
 ?>
