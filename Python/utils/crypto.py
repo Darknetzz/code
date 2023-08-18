5aa7dc0992891c0476e5255aeb0d81e26b1a9b4e
Kristian <github@roste.org>
2023-08-19 00:54:37 +0200

    automatic: push from codebase to github

---
+++ Python/utils/crypto.py
@@ -0,0 +1,33 @@
+import os
+from cryptography.fernet import Fernet
+
+class darkCrypt:
+    def __init__(self):
+        self.initialized = True
+        
+    def __readFile(self, fileName):
+        try:
+            with open(fileName) as f:
+                return f.read()
+        except Exception:
+            exit(f"Unable to read file {fileName}")
+
+    def secret_decrypt(self, secret, key):
+        if os.path.isfile(secret):
+            secret = self.__readFile(secret)
+        if os.path.isfile(key):
+            key = self.__readFile(key)
+
+        fernet = Fernet(key)
+        return fernet.decrypt(secret).decode()
+
+    def secret_encrypt(self, secret, key):
+        if os.path.isfile(secret):
+            secret = self.__readFile(secret)
+        if os.path.isfile(key):
+            key = self.__readFile(key)
+
+        fernet = Fernet(key)
+        return fernet.encrypt(secret).decode()
+
+# test123
