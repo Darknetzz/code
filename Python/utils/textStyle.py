9bf48965eb3e1af3381f35936600f42b19553948
Kristian <github@roste.org>
2023-08-19 02:10:13 +0200

    automatic: push from codebase to github

--- Python/utils/textStyle.py
+++ Python/utils/textStyle.py
@@ -7,13 +7,14 @@ COLORS = {
     "warning"  : "93m"  , # yellow [WARNING]
     "primary"  : "96m"  , # cyan  [INPUT, INFO etc.]
     "secondary": "1;30m", # grey
+    "end"      : "[00m",  # end
 }
 
 # ---------------------------------------------------------------------------- #
 #                               Text style functions                           #
 # ---------------------------------------------------------------------------- #
 def style(text, style = 'primary'):
-    return f"\033[{COLORS[style]}{text}\033[00m"
+    return f"\033[{COLORS[style]}{text}\033{COLORS['end']}"
 
 def inputs(text):
     prompt = input(style(text, "primary"))
