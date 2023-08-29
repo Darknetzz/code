7c37ee9466b207e97fc15bbb667019d5d16e2957
Kristian <github@roste.org>
2023-08-29 10:03:03 +0200

    automatic                : push from codebase to github

--- PowerShell/mount_NAS3.ps1
+++ PowerShell/mount_NAS3.ps1
@@ -1,3 +1,8 @@
+# This file is really of no use to the public, but I like to have it readily available to myself.
+
+# Remote execute:
+# pwsh -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/Darknetzz/code/main/PowerShell/mount_NAS3.ps1'))"
+
 $mountPoints = @{
     "Z" = "\\NAS3.dark.net\Share"
     "Y" = "\\ubuntu02.dark.net\data"
