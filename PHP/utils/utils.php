94947597c25ac458d8d1c405dfa1e068a0b72ca0
Kristian <github@roste.org>
2023-10-02 23:01:04 +0200

    automatic                : push from codebase to github

--- PHP/utils/utils.php
+++ PHP/utils/utils.php
@@ -1,35 +1,48 @@
 <?php
-
-
 /*
-──────────────────────────────────────────────────────────────────────────────────────────────────────────────
-[ HOW_TO_USE ] ################ [ HOW_TO_USE ] ################ [ HOW_TO_USE ] ################ [ HOW_TO_USE ]
-──────────────────────────────────────────────────────────────────────────────────────────────────────────────
+    # ────────────────────────────────────────────────────────── #
+    #                                                            #
+    #    $$\   $$\ $$$$$$$$\ $$$$$$\ $$\       $$$$$$\           #
+    #    $$ |  $$ |\__$$  __|\_$$  _|$$ |     $$  __$$\          #
+    #    $$ |  $$ |   $$ |     $$ |  $$ |     $$ /  \__|         #
+    #    $$ |  $$ |   $$ |     $$ |  $$ |     \$$$$$$\           #
+    #    $$ |  $$ |   $$ |     $$ |  $$ |      \____$$\          #
+    #    $$ |  $$ |   $$ |     $$ |  $$ |     $$\   $$ |         #
+    #    \$$$$$$  |   $$ |   $$$$$$\ $$$$$$$$\\$$$$$$  |         #
+    #     \______/    \__|   \______|\________|\______/          #
+    #        
+    # ────────────────────────────────────────────────────────── #
+    # ----[    General but useful PHP utilities. ]-------------  #
+    # ----[    Made with ❤️ by darknetzz         ]-------------  #
+    # ----[    https://github.com/Darknetzz/     ]-------------  #
+    # ────────────────────────────────────────────────────────── #
 
-# Import utils
-require_once("utils.php");
-use Darknetzz\PHPUtils as Utils;
-$util = new Utils;
+    # Usage:
+    # ===== Test =====
+    # Import utils
+    require_once("utils.php");
+    use Darknetzz\PHPUtils as Utils;
+    $util = new Utils;
 
-# Examples:
+    # Examples:
 
-- Slugify
-    $string     = "Some test-string_haha :D";
-    $slugified  = $util->slugify($string); 
-    echo $slugified; # should return "some_test_string_haha"
+    - Slugify
+        $string     = "Some test-string_haha :D";
+        $slugified  = $util->slugify($string); 
+        echo $slugified; # should return "some_test_string_haha"
 
 
-- countFunctionParams
-    $functionName = "strlen";
-    $countParams  = $util->countFunctionParams($functionName);
-    echo "$functionName has $countParams params.\n";
+    - countFunctionParams
+        $functionName = "strlen";
+        $countParams  = $util->countFunctionParams($functionName);
+        echo "$functionName has $countParams params.\n";
 
-- var_assert
-    $variable = "This is set!";
-    $variable2= "But this will not be..."; 
-    unset($variable2); 
-    echo "\$variable: ".$util->var_assert($variable."\n");  should be true    ("This is set!")
-    echo "\$variable2:".$util->var_assert($variable2."\n"); should be false   (null)
+    - var_assert
+        $variable = "This is set!";
+        $variable2= "But this will not be..."; 
+        unset($variable2); 
+        echo "\$variable: ".$util->var_assert($variable."\n");  should be true    ("This is set!")
+        echo "\$variable2:".$util->var_assert($variable2."\n"); should be false   (null)
 
 ──────────────────────────────────────────────────────────────────────────────────────────────────────────────
 [ END_HOWTO ] ################## [ END_HOWTO ] ################## [ END_HOWTO ] ################## [ END_HOWTO ]
@@ -39,10 +52,11 @@ $util = new Utils;
 namespace Darknetzz;
 
 
-// %%====== Test ======%%
-# %%====== Test ======%%
+# ==== TEST ==== //
+// ==== TEST ==== //
+// ==== TEST ==== //
+// ==== TEST ==== //
 
-#==== Hello world
 
 /* ────────────────────────────────────────────────────────────────────────── */
 /*                                 Class Utils                                */
