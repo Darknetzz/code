52246b8e86990fe5b1f34c1190f82c24db0d132e
Kristian Røste <github@roste.org>
2023-11-15 16:01:04 +0100

    automatic                : push from codebase to github

--- Python/github_watcher/github_watcher.py
+++ Python/github_watcher/github_watcher.py
@@ -14,18 +14,15 @@ def github_watcher(stream=False):
     outputfile  = "templates/output.html"
     
     with open(outputfile, "w+", encoding="UTF-8") as f:
-       console     = Console(record=True)
-       console.log(f"Initializing github_watcher...")
+        console     = Console(record=True)
+        console.log(f"Initializing github_watcher...")
 
-       # Empty output file
-       # with open(outputfile, "w+", encoding='UTF-8') as f:
-       #     f.write("")
-       
-       def stream(output: str):
-           yield output
+        def stream(output: str):
+            if complete == False:
+                yield output
        
        # Write to console as well
-       def write_output():
+        def write_output():
            return
            try:
                 # UnicodeEncodeError: 'charmap' codec can't encode character '\u280b' in position 313: character maps to <undefined>
@@ -35,20 +32,20 @@ def github_watcher(stream=False):
            except Exception as e:
                exit(f"Error in write_output: {e}")
            
-       def log(txt: str):
+        def log(txt: str):
            console.log(txt)
            write_output()
            
-       def warn(txt: str):
+        def warn(txt: str):
            log(f"[bold red][WARNING][/bold red] {txt}")
 
-       def succ(txt: str):
+        def succ(txt: str):
            log(f"[bold green][OK][/bold green] {txt}")
 
-       def info(txt: str):
+        def info(txt: str):
            log(f"[cyan][INFO][/cyan] {txt}")
 
-       def fetch(url: str):
+        def fetch(url: str):
            req = requests.get(url)
            return req
            return {
@@ -56,7 +53,7 @@ def github_watcher(stream=False):
                'url':     req.url
            }
 
-       def find(req: requests.Response, tagname: str, attributes: dict = {}):
+        def find(req: requests.Response, tagname: str, attributes: dict = {}):
            # Init soup
            try:
                soup = BeautifulSoup(req.content, "html.parser")
@@ -74,56 +71,56 @@ def github_watcher(stream=False):
                # warn(f"find: Exception {e}")
                return False
 
-       # Github repos to watch
-       repos = {
-           'Grocy'                         : 'https://github.com/grocy/grocy',
-           'wallpanel.xyz'                 : 'https://github.com/TheTimeWalker/wallpanel-android',
-           'Home Assistant: Variables'     : 'https://github.com/snarky-snark/home-assistant-variables',
-           'Home Assistant: Birthdays'     : 'https://github.com/Miicroo/ha-birthdays',
-           'Home Assistant: browser-mod'   : 'https://github.com/thomasloven/hass-browser_mod',
-           'php-api'                       : 'https://github.com/Darknetzz/php-api',
-           'php-rand'                      : 'https://github.com/Darknetzz/php-rand',
-           'JS.Wiki'                       : 'https://github.com/Requarks/wiki',
-           'Python Flask'                  : 'https://github.com/pallets/flask',
-           'Python Rich'                   : 'https://github.com/Textualize/rich',
-           'Windows Terminal'              : 'https://github.com/microsoft/terminal/',
-           'Spiderfoot'                    : 'https://github.com/smicallef/spiderfoot/',
-           'PHPMyAdmin'                    : 'https://github.com/phpmyadmin/phpmyadmin',
-           'DokuWiki'                      : 'https://github.com/dokuwiki/dokuwiki',
-           'Pi-Hole DNS'                   : 'https://github.com/pi-hole/pi-hole/',
-           'Pi-Hole FTL'                   : 'https://github.com/pi-hole/FTL/',
-           'Pi-Hole Web'                   : 'https://github.com/pi-hole/web',
-           'Bootstrap'                     : 'https://github.com/twbs/bootstrap',
-           'Obsidian.md'                   : 'https://github.com/obsidianmd/obsidian-releases',
-           'OPNSense Core'                 : 'https://github.com/opnsense/core',
-           'Apache2'                       : 'https://github.com/apache/httpd',
-           'Nginx'                         : 'https://github.com/nginx/nginx',
-           'Quasar RAT'                    : 'https://github.com/quasar/Quasar',
-           'Explorer Patcher'              : 'https://github.com/valinet/ExplorerPatcher/',
-           'VSCode'                        : 'https://github.com/microsoft/vscode',
-           'Flipper Zero RougeMaster FW'   : 'https://github.com/RogueMaster/flipperzero-firmware-wPlugins',
-           'Ventoy'                        : 'https://github.com/ventoy/Ventoy',
-           'SnipeIT Asset Management'      : 'https://github.com/snipe/snipe-it',
-       }
+        # Github repos to watch
+        repos = {
+            'Grocy'                         : 'https://github.com/grocy/grocy',
+            'wallpanel.xyz'                 : 'https://github.com/TheTimeWalker/wallpanel-android',
+            'Home Assistant: Variables'     : 'https://github.com/snarky-snark/home-assistant-variables',
+            'Home Assistant: Birthdays'     : 'https://github.com/Miicroo/ha-birthdays',
+            'Home Assistant: browser-mod'   : 'https://github.com/thomasloven/hass-browser_mod',
+            'php-api'                       : 'https://github.com/Darknetzz/php-api',
+            'php-rand'                      : 'https://github.com/Darknetzz/php-rand',
+            'JS.Wiki'                       : 'https://github.com/Requarks/wiki',
+            'Python Flask'                  : 'https://github.com/pallets/flask',
+            'Python Rich'                   : 'https://github.com/Textualize/rich',
+            'Windows Terminal'              : 'https://github.com/microsoft/terminal/',
+            'Spiderfoot'                    : 'https://github.com/smicallef/spiderfoot/',
+            'PHPMyAdmin'                    : 'https://github.com/phpmyadmin/phpmyadmin',
+            'DokuWiki'                      : 'https://github.com/dokuwiki/dokuwiki',
+            'Pi-Hole DNS'                   : 'https://github.com/pi-hole/pi-hole/',
+            'Pi-Hole FTL'                   : 'https://github.com/pi-hole/FTL/',
+            'Pi-Hole Web'                   : 'https://github.com/pi-hole/web',
+            'Bootstrap'                     : 'https://github.com/twbs/bootstrap',
+            'Obsidian.md'                   : 'https://github.com/obsidianmd/obsidian-releases',
+            'OPNSense Core'                 : 'https://github.com/opnsense/core',
+            'Apache2'                       : 'https://github.com/apache/httpd',
+            'Nginx'                         : 'https://github.com/nginx/nginx',
+            'Quasar RAT'                    : 'https://github.com/quasar/Quasar',
+            'Explorer Patcher'              : 'https://github.com/valinet/ExplorerPatcher/',
+            'VSCode'                        : 'https://github.com/microsoft/vscode',
+            'Flipper Zero RougeMaster FW'   : 'https://github.com/RogueMaster/flipperzero-firmware-wPlugins',
+            'Ventoy'                        : 'https://github.com/ventoy/Ventoy',
+            'SnipeIT Asset Management'      : 'https://github.com/snipe/snipe-it',
+        }
 
-       # Fetch from file to see if updated
-       current = {}
-       filepath= os.path.dirname(__file__)
-       file    = os.path.basename(__file__.split('.')[0])+'.json'
-       fullpath= os.path.join(f"{filepath}/{file}")
-       if os.path.isfile(fullpath):
-           with open(fullpath, 'r') as fcontents:
-               current = json.load(fcontents)
+        # Fetch from file to see if updated
+        current = {}
+        filepath= os.path.dirname(__file__)
+        file    = os.path.basename(__file__.split('.')[0])+'.json'
+        fullpath= os.path.join(f"{filepath}/{file}")
+        if os.path.isfile(fullpath):
+            with open(fullpath, 'r') as fcontents:
+                current = json.load(fcontents)
 
-       info(f"Checking file {fullpath}")
+        info(f"Checking file {fullpath}")
 
-       headers = ['Name', 'Current tag', 'Last tag', 'Date', 'New']
-       rows    = []
-       toadd   = {}
-       changes = False
-       count   = len(repos)
+        headers = ['Name', 'Current tag', 'Last tag', 'Date', 'New']
+        rows    = []
+        toadd   = {}
+        changes = False
+        count   = len(repos)
 
-       with console.status("[bold]Fetching repos...") as status:
+        with console.status("[bold]Fetching repos...") as status:
            for i, repo in enumerate(repos):
                # Fetch latest release for this repo
                url     = repos[repo]
@@ -191,28 +188,28 @@ def github_watcher(stream=False):
                rows.append(values)
                time.sleep(0.5)
 
-       log('\n\n')
+        log('\n\n')
 
-       table = Table(title="GitHub Repo Overview")
-
-       for header in headers:
-           table.add_column(header)
-
-       for row in rows:
-               table.add_row(*row)
-
-       log(table)
-
-       if changes != True:
-           info("No new releases, exiting...")
-           return
-
-       save = input(f"Save these tags to {file}? [Y/n]")
-       if save.lower() != 'y' and save != '':
-           info("Exiting...")
-           return
-
-       with open(fullpath, 'w+') as fcontents:
+        table = Table(title="GitHub Repo Overview")
+    
+        for header in headers:
+            table.add_column(header)
+    
+        for row in rows:
+                table.add_row(*row)
+    
+        log(table)
+    
+        if changes != True:
+            info("No new releases, exiting...")
+            return
+    
+        save = input(f"Save these tags to {file}? [Y/n]")
+        if save.lower() != 'y' and save != '':
+            info("Exiting...")
+            return
+    
+        with open(fullpath, 'w+') as fcontents:
            fcontents.write(json.dumps(toadd))
            succ("File saved!")
         
