f81a69b7f2b6c6e8605265bb32155970c0e05b97
Kristian Røste <github@roste.org>
2023-12-27 13:02:33 +0100

    automatic                : push from codebase to github

--- PowerShell/mount_drives.ps1
+++ PowerShell/mount_drives.ps1
@@ -3,6 +3,37 @@
 # Remote execute (you can add this in task scheduler):
 # pwsh.exe -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/Darknetzz/code/main/PowerShell/mount_drives.ps1'));"
 
+
+
+<#
+.SYNOPSIS
+    Retrieves the credentials for mounting drives.
+
+.DESCRIPTION
+    The getCredentials function retrieves the credentials required for mounting drives. It first attempts to retrieve the credentials with the specified credential name ("DARK.NET"). If the credentials are not found, it falls back to retrieving the credentials for the current user.
+
+.OUTPUTS
+    System.Management.Automation.PSCredential
+        The retrieved credentials.
+
+.EXAMPLE
+    $credentials = getCredentials
+    # Retrieves the credentials for mounting drives.
+
+#>
+function getCredentials($credentialName = "DARK.NET") {
+    
+    do {
+        $credential = Get-Credential -ErrorAction SilentlyContinue -UserName $credentialName
+        if ($credential -ne $null) { break; }
+        $credential = Get-Credential -ErrorAction SilentlyContinue -UserName $env:USERNAME
+    } while ($False)
+
+    return $credential
+}
+
+$credential = getCredentials
+
 $mountPoints = @{
     "X" = "\\NAS1.dark.net\Data"
     "Y" = "\\NAS2.dark.net\Data"
@@ -20,10 +51,19 @@ foreach ($mp in $iterator) {
     If (Get-PSDrive | Where-Object DisplayRoot -EQ $uncPath) { 
         Write-Output "$uncPath already mapped.";
     } else {
+        
         $cmd = "net use ${letter}: ${uncPath} /persistent:yes /yes /user:$env:USERNAME"
+        $cmd = "net use ${letter}: ${uncPath} /persistent:yes /yes /user:$($credential.UserName)"
 
         Write-Output "Mounting $uncPath to $letter";
-        Start-Process -FilePath "pwsh.exe" -ArgumentList "-Command", "$cmd" -Wait -Timeout 10
+        $timeoutSeconds = 10
+        $process = Start-Process -FilePath "pwsh.exe" -ArgumentList "-Command", "$cmd" -PassThru
+        $process.WaitForExit($timeoutSeconds * 1000)
+        if (-not $process.HasExited) {
+            $process.Kill()
+            Write-Output "Process timed out."
+        }
+        
     }
 }
 
